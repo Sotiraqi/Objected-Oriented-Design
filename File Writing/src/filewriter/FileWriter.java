4f5a27a0c62b7a4db7f744c0b97edff3d5212e9c
Sotiraqi <72278850+Sotiraqi@users.noreply.github.com>
2021-03-19 16:53:19 +0100

    Add files via upload

---
+++ File Writing/src/filewriter/FileWriter.java
@@ -0,0 +1,53 @@
+package filewriter;
+
+import java.util.Scanner;
+import java.io.*;
+
+public class FileWriter {
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		BufferedWriter bwriter = null;
+		try 
+		{
+			bwriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:\\IA\\File Writing\\file_out.txt")));
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("File does not exist!");
+			e.printStackTrace();
+		}
+		
+		System.out.println("Please input words: ");
+		String word = input.next();
+		
+		while(word != "end")
+		{
+			try 
+			{
+				bwriter.write(word);
+			} 
+			catch (IOException e) 
+			{
+				System.out.println("Word can't be writed");
+				e.printStackTrace();
+			}
+			
+			word = input.next();
+		}
+		
+		try 
+		{
+			bwriter.close();
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("Cannot close file!");
+			e.printStackTrace();
+		}
+		input.close();
+
+	}
+
+}
